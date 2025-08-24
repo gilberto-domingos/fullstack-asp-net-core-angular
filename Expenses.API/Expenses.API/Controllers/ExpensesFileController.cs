dd50a7ed48f9efcf692f17f8a6307b19103c139b
gilberto-domingos <gilberto1domingos1@outlook.com>
2025-08-24 12:45:45 -0300

    config: program (#72)

---
+++ Expenses.API/Expenses.API/Controllers/ExpensesFileController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ExpensesFileController : ControllerBase
+{
+    private readonly FileDataService _service = new();
+
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        var expenses = _service.GetAll();
+        return Ok(expenses);
+    }
+
+    [HttpPost("Teste")]
+    public IActionResult Add([FromBody] Expense expense)
+    {
+        _service.Add(expense);
+        return Ok(expense);
+    }
+}
