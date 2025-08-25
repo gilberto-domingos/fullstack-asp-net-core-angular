f5a81f8d0ac9022b5edf1e3804d0ebc38059410e
gilberto-domingos <gilberto1domingos1@outlook.com>
2025-08-25 12:40:18 -0300

    feat: add spinner3 login (#76)

--- Expenses.Client/src/app/app.ts
+++ Expenses.Client/src/app/app.ts
@@ -1,14 +1,29 @@
-import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, signal } from '@angular/core';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { RouterOutlet } from '@angular/router';
 import { Footer } from './components/footer/footer';
 import { Header } from './components/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer],
+  standalone: true,
+  imports: [RouterOutlet, Header, Footer, CommonModule, MatProgressSpinnerModule],
   templateUrl: './app.html',
-  styleUrl: './app.scss',
+  styleUrls: ['./app.scss'],
 })
-export class App {
+export class App implements OnInit {
+  isLoading = signal(true);
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get('https://fullstack-6-40oe.onrender.com/api/Students/All').subscribe({
+      next: () => this.isLoading.set(false),
+      error: () => this.isLoading.set(false),
+    });
+  }
+
   protected readonly title = signal('Expenses.Client');
 }
